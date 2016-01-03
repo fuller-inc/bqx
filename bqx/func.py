d41faa6e85c2972612a774fabcda8aa32a4d4855
puhitaku <puhitaku@gmail.com>
2016-01-03 21:39:19 +0900

    Add clever func factory and math funcs

--- bqx/func.py
+++ bqx/func.py
@@ -9,13 +9,10 @@ def CONCAT(*args):
     arg = ', '.join(arg)
     return Column('CONCAT(%s)' % arg)
 
-def IF(cond, val_t, val_f):
-    cond, val_t, val_f = (_actual_n(x) for x in [cond, val_t, val_f])
-    return Column('IF(%s, %s, %s)' % (cond, val_t, val_f))
-
 def _fn_factory(name):
-    return lambda col: Column('{0}({1})'.format(name, _actual_n(col)))
-    #return eval("lambda col: Column('{0}(%s)' % _actual_n(col))".format(name))
+    def _fn(*col):
+        return Column('{0}({1})'.format(name, ','.join(_actual_n(x) for x in col)))
+    return _fn
 
 def _actual_n(col):
     if isinstance(col, str):
@@ -28,3 +25,6 @@ def _actual_n(col):
 COUNT = _fn_factory('COUNT')
 SUM = _fn_factory('SUM')
 INTEGER = _fn_factory('INTEGER')
+SQRT = _fn_factory('SQRT')
+IF = _fn_factory('IF')
+POW = _fn_factory('POW')
